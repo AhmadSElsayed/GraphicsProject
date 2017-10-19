1bc68f72fc3b24d5743d75ad9da15dd7773154d2
AhmadSElsayed <ahmad.hassanien96@eng-st.cu.edu.eg>
2017-10-19 11:14:37 +0200

    New install Shell

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install libgl1-mesa-dev mesa-common-dev freeglut3 freeglut3-dev libxrandr-dev cmake libglew-dev libglm-dev libassimp-dev libbullet-dev libsoil-dev libglfw3-dev
+cmake CMakeLists.txt
+make
+./Loader
